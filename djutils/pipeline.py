a4e9e9da35192816aca867a8aae47ae87abd6069
shenshan <shenshanpku@gmail.com>
2020-05-15 02:06:51 -0700

    Add hookup in pipeline, get rid of requirements

--- djutils/pipeline.py
+++ djutils/pipeline.py
@@ -26,16 +26,15 @@ class Pipeline:
 
         return msg
 
-    def _check_requirements(self, requirements=None):
-        if not requirements:
-            raise KeyError(self.requirements)
+    def _check_requirements(self, requirements):
 
         checked_requirements = {}
-        for k in self.requried_table_names:
-            if k not in requirements:
-                raise KeyError(f'Requiring upstream table: {k}')
-            else:
-                checked_requirements[k] = requirements[k]
+        if self.requried_table_names:
+            for k in self.requried_table_names:
+                if k not in requirements:
+                    raise KeyError(f'Requiring upstream table: {k}')
+                else:
+                    checked_requirements[k] = requirements[k]
 
         if self.requried_method_names:
             for k in self.requried_method_names:
@@ -46,24 +45,28 @@ class Pipeline:
 
         return checked_requirements
 
-    def _init_tables(self, schema, context):
+    def init_pipeline(self, schema, requirements=None, context={}, add_here=False):
+
+        requirements = self._check_requirements(requirements)
+
+        if add_here and not context:
+            context = inspect.currentframe().f_back.f_locals
+
         tables = {}
         for table_class in self._table_classes:
+
+            if requirements:
+                for hook_name, hook_target in requirements.items():
+                    hook_name = f'_{hook_name}'
+                    if hook_name in dir(table_class):
+                        setattr(table_class, hook_name, hook_target)
+
             print(f'Initializing {table_class.__name__}')
             table = schema(table_class, context=context)
             context[table.__name__] = table
             tables[table.__name__] = table
 
-        return tables
-
-    def init_pipeline(self, schema, context={}, add_here=False):
-        if add_here and not context:
-            context = inspect.currentframe().f_back.f_locals
-
-        tables = self._init_tables(schema, context)
-        pipeline = {**tables}
-
         if add_here:
-            context.update(**pipeline)
+            context.update(**tables)
 
-        return pipeline
+        return tables
